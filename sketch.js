58d8cf832aef527725fa8800f311317bbaba75e4
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-05 10:59:32 -0500

    Ellipse fixed -_-

--- sketch.js
+++ sketch.js
@@ -3,5 +3,8 @@ function setup() {
   }
   
   function draw() {
-    background(220);
+    background(220); //Set background color (RGB)
+    fill(0);
+    ellipse(width/2, height/2, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
+    ellipse(200, 200, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
   }
