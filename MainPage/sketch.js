da4e35e97646423ceeffef73bcb26df8356d9174
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-11 23:03:18 -0500

    Figured out how to change pages with a link!
    I found a way to switch to a different page by using links. I also had to link the new html page to its own js and css files.

--- MainPage/sketch.js
+++ MainPage/sketch.js
@@ -17,3 +17,5 @@ function setup() {
     ellipse(width/2, height/2, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
     ellipse(200, 200, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
   } */
+
+    
