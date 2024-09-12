6651cfee4ce3e91cbfefd0dcb99f1a69417370f1
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-12 10:07:14 -0500

    Set up 3 pages and link them to main
    All three html pages now have their own js and css files. Im not sure why the image isnt showing up now though.

--- ItemOnePage/item-one.js
+++ ItemOnePage/item-one.js
@@ -1,1 +1,13 @@
-console.log("it worked");
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight); //Create a canvas that is the size of the window
+    CanvasCaptureMediaStreamTrack.position(0, 0); //Set the position of the canvas to (0, 0)
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+  }
+
+  function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight); //Resize the canvas to the size of the window
+    mouseX = width/2;
+    mouseY = height/2;
+  }
+
---
+++ ItemTwoPage/item-two.js
@@ -0,0 +1,12 @@
+// item-two.js
+
+// Your code goes here
+
+// Example code:
+console.log("Hello, world!");
+
+// Export any functions or variables if needed
+// module.exports = {
+//   functionName: functionName,
+//   variableName: variableName
+// };
