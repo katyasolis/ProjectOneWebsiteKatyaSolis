c24283102ef7fed3971d6e7a8680e7c7a6fa026e
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-12 10:48:03 -0500

    I fixed the images

--- MainPage/sketch.js
+++ MainPage/sketch.js
@@ -1,3 +1,6 @@
+let Camera = "cameraObject";
+
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight); //Create a canvas that is the size of the window
     CanvasCaptureMediaStreamTrack.position(0, 0); //Set the position of the canvas to (0, 0)
@@ -11,11 +14,6 @@ function setup() {
     mouseY = height/2;
   }
   
-  /*function draw() {
-    background(220); //Set background color (RGB)
-    fill(0);
-    ellipse(width/2, height/2, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
-    ellipse(200, 200, 50, 50); //Draw an ellipse at (200, 200) with width and height of 50
-  } */
+
 
     
