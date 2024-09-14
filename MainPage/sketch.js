108e058afdecd57bfafe2d9e03d393d71caa91ff
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-13 21:40:47 -0500

    not there yet with the buttons

--- MainPage/sketch.js
+++ MainPage/sketch.js
@@ -1,19 +1,28 @@
-let Camera = "cameraObject";
 
+
+function preload() {
+  Camera = loadImage("/Images/Camera Item.png"); // Load the image
+}
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight); //Create a canvas that is the size of the window
-    CanvasCaptureMediaStreamTrack.position(0, 0); //Set the position of the canvas to (0, 0)
+    canvas.position(0, 0); //Set the position of the canvas to (0, 0)
     canvas.style('z-index', '-1');
     canvas.style('position', 'fixed');
+
+    let Camera = select("#cameraObject");
+
+    Camera.mousePressed(() => {
+      window.location.href = "http://127.0.0.1:3000/ItemOnePage/item-one.html";
+    });
   }
+
 
   function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight); //Resize the canvas to the size of the window
     mouseX = width/2;
     mouseY = height/2;
   }
-  
 
 
-    
+
