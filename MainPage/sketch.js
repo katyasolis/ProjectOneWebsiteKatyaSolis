b9a075f22e8c228c06f5c907695db7d5701e8527
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-16 10:56:48 -0500

    all images and links ready

--- MainPage/sketch.js
+++ MainPage/sketch.js
@@ -11,15 +11,33 @@ function setup() {
     canvas.style('position', 'fixed');
 
     let Camera = select("#cameraObject");
+    let Mirror = select("#mirrorObject");
+    let Shell = select("#shellObject");
 
     Camera.mousePressed(() => {
       goToDifferentPage();
+    });
+
+    Mirror.mousePressed(() => {
+      goToDifferentPageTwo();
+    });
+
+    Shell.mousePressed(() => {
+      goToDifferentPageThree();
     });
   }
 
   function goToDifferentPage(){
     window.location.href = "http://127.0.0.1:3000/ItemOnePage/item-one.html";
   }
+
+  function goToDifferentPageTwo(){
+    window.location.href = "http://127.0.0.1:3000/ItemTwoPage/item-two.html";
+  }
+
+  function goToDifferentPageThree(){
+    window.location.href = "http://127.0.0.1:3000/ItemThreePage/item-three.html";
+  } 
 
   function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight); //Resize the canvas to the size of the window
